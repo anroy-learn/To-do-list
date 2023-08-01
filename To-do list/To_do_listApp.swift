4e488c43ba55756d8d415d13864756dbfa961944
anroy-learn <98684225+anroy-learn@users.noreply.github.com>
2023-08-01 13:41:05 -0400

    Initial Commit

---
+++ To-do list/To_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_listApp.swift
+//  To-do list
+//
+//  Created by Angel Roy on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct To_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
